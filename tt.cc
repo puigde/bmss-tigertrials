ef633e736bbf25217134835b08d3d4fc1205ccd9
polpuigdemont <polpuigdemont@gmail.com>
2022-03-15 14:27:59 +0100

    fixed complexity analysis and function documentation

--- tt.cc
+++ tt.cc
@@ -5,11 +5,12 @@ using M = vector<vector<int>>;
 M mem;
 
 // Dynamic programming bottom-up implementation which fills up the memoization global table mem with the
-// maximum subset sum that can be achieved up to the i-th element of the subset vector specified as a
-// parameter under the condition that it doesn't overcome the bound b which is also specified as a parameter
-// Complexity is O(n^2) with n being the number of elements of the subset, NOTE that subset does not have any
-// padding so for referencing the i-th element we use the indexing i-1
-double max_subset_sum(int n, int b, const vector<int> &subset)
+// maximum sum of the elements in a subset non strictly lower than a bound b, specified as a parameter,
+// that can be achieved using up to the i-th element of the subset vector specified as a parameter
+// Complexity is O(n*b) with n being the number of elements of the subset and b the mentioned bound
+// NOTE that subset does not have any padding so for referencing the i-th element referenced 1,...,n
+// we use the indexing i-1 because we are referencing 0,...,n-1
+double bounded_maximum_sum_subset(int n, int b, const vector<int> &subset)
 {
     for (int i = 1; i <= n; i++)
     {
@@ -24,9 +25,9 @@ double max_subset_sum(int n, int b, const vector<int> &subset)
     return mem[n][b];
 }
 
-// Gets the values of the those involved in the maxsubsetsum stored in the global table mem
-// Complexity is O(n)
-vector<int> get_ms_elements(int n, int b, const vector<int> subset)
+// Gets the values of the elements involved in the maxsubsetsum stored in the global table mem
+// Complexity is O(w+n)
+vector<int> get_bmss_elements(int n, int b, const vector<int> subset)
 {
     int i = n;
     int j = b;
@@ -53,6 +54,6 @@ int main()
     vector<int> subset(n);
     for (int &x : subset)
         cin >> x;
-    int solucio = max_subset_sum(n, b, subset);
-    vector<int> values = get_ms_elements(n, b, subset);
+    int solucio = bounded_maximum_sum_subset(n, b, subset);
+    vector<int> values = get_bmss_elements(n, b, subset);
 }
